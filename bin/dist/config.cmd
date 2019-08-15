3f84f57a95a913ac416fa41a3c19a981a556684b
wuding <netjoin@gmail.com>
2019-08-15 15:14:28 +0800

    修改为中文注释，添加 ping 子域名

--- bin/dist/config.cmd
+++ bin/dist/config.cmd
@@ -1,16 +1,16 @@
 set drive_letter=E
 
-REM 版本号
+REM version
 set php_v=7.4
 set php_ver=%php_v%.0beta2
 
-REM 参数
+REM argument
 set php_addr=0.0.0.0
 set php_port=80
 set php_docroot=%drive_letter%:/env/www
 set php_router=
 
-REM 目录
+REM directory
 set temp_dir=%drive_letter%:\env\tmp
 set soft_dir=%drive_letter%:\env\win\ProgramFiles
 
--- bin/dist/ping.cmd
+++ bin/dist/ping.cmd
@@ -1,1 +1,4 @@
 set ping_hosts=urlnk.com,urlnk.cc,urlnk.io,urlnk.host
+set ping_domain=urlnk.com
+set ping_subdomains=www,w,a,wan,lan,wlan,loc
+set ping_count=1

c7e797976de3b95be42422df2c32a2e5a2df116f
wuding <netjoin@gmail.com>
2019-08-16 22:17:57 +0800

    新增文档自述文件，使用特别查询调用功能

--- www/index.php
+++ www/index.php
@@ -1,14 +1,20 @@
 <?php
 define('ROOT_DIR', isset($_ENV['root_dir']) ? $_ENV['root_dir'] : $_SERVER['root_dir']);
+define('QUERY_SPEC', isset($_GET['!']) ? explode(',', $_GET['!']) : array());
 
-include ROOT_DIR . '/src/Links.php';
-$config = include ROOT_DIR . '/etc/config.php';
-extract($config);
-
-if (isset($_GET['phpinfo'])) {
+if (in_array('phpinfo', QUERY_SPEC)) {
     phpinfo();
 }
 
-$urls = \Env\Links::url($domains, $subdomains);
-$html = \Env\Links::html($urls);
-print_r($html);
+if (in_array('links', QUERY_SPEC)) {
+	include ROOT_DIR . '/src/Links.php';
+	$config = include ROOT_DIR . '/etc/config.php';
+	extract($config);
+
+	$urls = \Env\Links::url($domains, $subdomains);
+	$html = \Env\Links::html($urls);
+	print_r($html);
+	exit;
+}
+
+# include ROOT_DIR .'/www\work\wuding\geminiphp\web\index.php';

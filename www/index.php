bde2fcce622c48f6aa1bc0152a90ecb2de27f644
wuding <netjoin@gmail.com>
2019-08-18 21:11:03 +0800

    修正 PHP 5.4 常量值不可以是数组类型

--- www/index.php
+++ www/index.php
@@ -1,13 +1,16 @@
 <?php
+global $_CONST;
+$_CONST = array();
+$_CONST['QUERY_SPEC'] = isset($_GET['!']) ? explode(',', $_GET['!']) : array();
+
 define('ROOT_DIR', dirname(__DIR__));
-define('QUERY_SPEC', isset($_GET['!']) ? explode(',', $_GET['!']) : array());
 define('QUERY_APP', isset($_GET['@']) ? $_GET['@'] : 'astrophp');
 
-if (in_array('phpinfo', QUERY_SPEC)) {
+if (in_array('phpinfo', $_CONST['QUERY_SPEC'])) {
     phpinfo();
 }
 
-if (in_array('links', QUERY_SPEC)) {
+if (in_array('links', $_CONST['QUERY_SPEC'])) {
 	include ROOT_DIR . '/src/Links.php';
 	$config = include ROOT_DIR . '/etc/config.php';
 	extract($config);

9d566e5f8819d09d875772c0605e003c0ae9f418
wuding <netjoin@gmail.com>
2019-08-19 16:44:16 +0800

    添加 php 路由文件配置

--- bin/dist/config.cmd
+++ bin/dist/config.cmd
@@ -2,9 +2,9 @@ set drive_letter=E
 set root_dir=%drive_letter%:\env
 
 REM version
-set php_v=7.4
-set php_ver=%php_v%.0beta2
-set nginx_ver=1.17.3
+set php_v=5.4
+set php_ver=%php_v%.45
+set nginx_ver=1.16.1
 
 REM option
 set php_bat=xxfpm
@@ -17,7 +17,7 @@ set php_port=8002,80
 ::set php_listen=192.168.100.4:80,127.0.0.1:8000
 set php_listen=
 set php_docroot=%root_dir%/www
-set php_router=
+set php_router=%root_dir%/www/router.php
 set php_bind=127.0.0.1:9000,192.168.100.4:9001
 set php_cgi_addr=127.0.0.1
 set php_cgi_port=9002,9003
@@ -36,3 +36,4 @@ REM configuration
 set nginx_conf=%data_dir%\nginx\conf\nginx.conf
 ::set nginx_conf=conf/nginx.conf
 set php_ini=%php_dir%/php.ini
+::set php_ini=%data_dir%/php/php.ini

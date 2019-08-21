eafbb0c9b665ef31fd0ae5f02543726ccb2a5bbd
wuding <netjoin@gmail.com>
2019-08-21 13:36:20 +0800

    添加配置文件路径

--- bin/php_cli_server.bat
+++ bin/php_cli_server.bat
@@ -5,9 +5,9 @@ echo Web Server
 REM https://www.php.net/manual/zh/features.commandline.webserver.php
 
 FOR %%G IN (%php_port%) DO (
-    start %php_dir%\php.exe -S %php_addr%:%%G -t %php_docroot% %php_router%
+    start %php_dir%\php.exe -c %php_ini% -S %php_addr%:%%G -t %php_docroot% %php_router%
 )
 
 FOR %%G IN (%php_listen%) DO (
-    start %php_dir%\php.exe -S %%G -t %php_docroot% %php_router%
+    start %php_dir%\php.exe -c %php_ini% -S %%G -t %php_docroot% %php_router%
 )
